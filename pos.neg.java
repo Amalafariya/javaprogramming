3be1b56ae21a14990c5453f9123b7f10b144322e
Amalafariya <43032743+Amalafariya@users.noreply.github.com>
2018-10-03 12:34:06 +0530

    Add files via upload

---
+++ pos.neg.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int number;
+    number=3;
+    if (number%2==0)
+    System.out.println(number + "even");
+    else if (number%2!= 0)
+    System.out.println(number + "odd");
+    else
+    System.out.println(number + "is zero");
+  }
+}
